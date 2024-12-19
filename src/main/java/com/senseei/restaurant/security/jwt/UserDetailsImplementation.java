4f92363d9659ab884275f7627aa95ca28b2b0e71
Gabriel de Jesus <gabrieldjesus475@gmail.com>
2024-12-19 12:36:07 -0300

    feat: added User Details implementation class

---
+++ src/main/java/com/senseei/restaurant/security/jwt/UserDetailsImplementation.java
@@ -0,0 +1,39 @@
+package com.senseei.restaurant.security.jwt;
+
+import com.senseei.restaurant.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImplementation implements UserDetails {
+    private final User user;
+
+    public UserDetailsImplementation(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
