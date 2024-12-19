3980c4853b763f8f0d8a1260e33b6349553c59fb
Gabriel de Jesus <gabrieldjesus475@gmail.com>
2024-12-19 12:36:11 -0300

    eat: added User Details and custom user details service implementation class

---
+++ src/main/java/com/senseei/restaurant/services/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.senseei.restaurant.services;
+
+import com.senseei.restaurant.entities.User;
+import com.senseei.restaurant.repositories.UserRepository;
+import com.senseei.restaurant.security.jwt.UserDetailsImplementation;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserDetailsImplementation(user);
+    }
+}
